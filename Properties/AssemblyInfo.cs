f3c9e8ce08a55228ed4fcc2ee9c6e83e34ad0486
aviad.naamat <aviad.naamat@hcl.com>
2022-08-18 13:32:46 +0300

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+/******************************************************************
+* Licensed Materials - Property of HCL
+* (c) Copyright HCL Technologies Ltd. 2007, 2010.
+* Note to U.S. Government Users Restricted Rights.
+******************************************************************/
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using AppScanVersionInfo;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PyScan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(AppScanVersionStrings.APPSCAN_COMPANY)]
+[assembly: AssemblyProduct(AppScanVersionStrings.APPSCAN_PRODUCT)]
+[assembly: AssemblyCopyright(AppScanVersionStrings.APPSCAN_COPYRIGHT)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f52f433-d4b8-44e5-bd4d-99e7e1ba1936")]
+
+// Version information for an assembly
+[assembly: AssemblyVersion(AppScanVersionStrings.APPSCAN_BIN_VERSION)]
+[assembly: AssemblyFileVersion(AppScanVersionStrings.APPSCAN_BIN_VERSION)]
+[assembly: AssemblyInformationalVersion(AppScanVersionStrings.APPSCAN_PRODUCT_VERSION)]
