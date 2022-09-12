b965663e3328cde1c646fad163ac8ae99811ba7e
aviadHCL <aviad.naamat@hcl.com>
2022-09-12 17:13:06 +0300

    Update copyright to all source code

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
-/******************************************************************
-* Licensed Materials - Property of HCL
-* (c) Copyright HCL Technologies Ltd. 2007, 2010.
-* Note to U.S. Government Users Restricted Rights.
-******************************************************************/
+/*#######################################################################################################################
+# Licensed Materials –Property of HCL Technologies Ltd.
+# © Copyright HCL Technologies Ltd. 2022.
+# All rights reserved. See product license for details. US Government Users Restricted Rights. Use, duplication,
+# or disclosure restricted by GSA ADP Schedule Contract with HCL Technologies Ltd. Java and all Java-based trademarks
+# and logos are trademarks or registered trademarks of Oracle and/or its affiliates. HCL, the HCL logo,
+# and Tivoli are registered trademarks of HCL Technologies in the United States, other countries, or both.
+#######################################################################################################################*/
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
--- StandAlone/PyScanConsole.cs
+++ StandAlone/PyScanConsole.cs
@@ -1,13 +1,11 @@
-/******************************************************************
-* IBM Confidential
-* OCO Source Materials
-* IBM Rational AppScan Standard Edition
-* (c) Copyright IBM Corp. 2007, 2010.
-* 
-* The source code for this program is not published or otherwise
-* divested of its trade secrets, irrespective of what has been
-* deposited with the U.S. Copyright Office.
-******************************************************************/
+/*#######################################################################################################################
+# Licensed Materials –Property of HCL Technologies Ltd.
+# © Copyright HCL Technologies Ltd. 2022.
+# All rights reserved. See product license for details. US Government Users Restricted Rights. Use, duplication,
+# or disclosure restricted by GSA ADP Schedule Contract with HCL Technologies Ltd. Java and all Java-based trademarks
+# and logos are trademarks or registered trademarks of Oracle and/or its affiliates. HCL, the HCL logo,
+# and Tivoli are registered trademarks of HCL Technologies in the United States, other countries, or both.
+#######################################################################################################################*/
 
 using System;
 using System.Collections.Generic;
